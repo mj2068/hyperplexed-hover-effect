1660e2cbbb8539f2ba5b18d4593b00834e388064
mj2068 <yuelianghushenfu@sina.com>
2022-11-08 15:05:07 +0800

    couldn't do it blind(means not look the tutorial)

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.getElementById("cards").onmousemove = (e) => {
-  for (const card of document.getElementsByClassName("card")) {
-    const rect = card.getBoundingClientRect(),
-      x = e.clientX - rect.x,
-      y = e.clientY - rect.y;
+  for (const card of document.querySelectorAll(".card")) {
+    let rect = card.getBoundingClientRect();
+    let x = e.clientX - rect.x;
+    let y = e.clientY - rect.y;
 
     card.style.setProperty("--mouse-x", `${x}px`);
     card.style.setProperty("--mouse-y", `${y}px`);
